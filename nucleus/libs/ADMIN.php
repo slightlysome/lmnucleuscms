31426dc9e33d14079afdaf8267b98f3910e3e049
yama <yamamoto@kyms.jp>
2014-06-23 08:52:16 +0900

    Fix - Delete unnecessary spaces before output

--- nucleus/libs/ADMIN.php
+++ nucleus/libs/ADMIN.php
@@ -5315,8 +5315,7 @@ selector();
 			$CONF['AdminCSS'] = 'original';
 		}
 
-		?>
-		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+		?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 		<html <?php echo _HTML_XML_NAME_SPACE_AND_LANG_CODE; ?>>
 		<head>
 			<meta http-equiv="Content-Type" content="text/html; charset=<?php echo _CHARSET ?>" />
