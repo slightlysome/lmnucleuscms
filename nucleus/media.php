c5991e155c6ebf4239481d30df054371084a308e
yama <yamamoto@kyms.jp>
2014-06-23 08:49:35 +0900

    Improved - Output Content-Type header

--- nucleus/media.php
+++ nucleus/media.php
@@ -400,6 +400,7 @@ function media_head() {
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=<?php echo _CHARSET ?>" />
 		<title>Nucleus Media</title>
 		<link rel="stylesheet" type="text/css" href="styles/popups.css" />
 		<script type="text/javascript">
